1746c841f5c24467cba7971186d41ddf23c84934
SamanthaC13 <sc3z7@umsystem.edu>
2021-11-03 18:19:29 -0500

    Added read File to parser

--- parser.c
+++ parser.c
@@ -4,11 +4,108 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"scanner.h"
-#include"driver.h"
 #include"parser.h"
-
-void parser()
+int ignore;
+int lineNum;
+FILE* input;
+void parser(char* filename)
 {
+	//file is opened for reading 
+	input=fopen(filename,"r");
+	if(input==NULL)
+	{
+		//Error is printed if input file is  unable to be opened
+		fprintf(stderr,"Error opening input file in TestScanner");
+		exit(1);
+	}
+	char* line=malloc(len*sizeof(char));
+	//read first line from file
+	line=fgets(line,len,input);
+	//send line to filter to filter out comments
+	line=filterLine(line);
+	int lineNum=1;
+	int startChar=0;
 	
+	//if it passes the parser send it to print the tree
+	
+}
+char* getNewLine(char* line)
+{
+	char* lastLine=line;
+	line=fgets(line,len,input);
+	if(feof(input))
+	{//if the current line was the last line then the lastLine variable will not be the line again and the next token gotten will be the EOFTK
+		line=lastLine;
+		startChar=startChar+1;
+	}
+	else
+	{
+		//send line to filter to filter out comments
+		line=filterLine(line);
+		lineNum=lineNum++;
+		startChar=0;
+	}
+	return line;
+}
+char* filterLine(char* line)
+//This function takes the line from the file and filters it into a newLine without comments
+{
+	//Count for the orginal line
+	int lineCharCount=0;
+	//count for the new created line
+	int newLineCharCount=0;
+	//flag for if there is a comment in this line
+	int commentFlag=0;
+	//c is the currrnet character from the orginal line
+	char c=line[lineCharCount];
+	char* newLine=malloc(sizeof(char)*124);
+	while(c!='\n')
+	//want to read the orginal line til it ends with the \n character
+	{
+		if(c=='&')
+		//If we get one apersand then we look to see if the next character is an apersand
+		{
+			lineCharCount++;
+			if(line[lineCharCount]=='&')
+			//if we found a second apersand then we need to figure out if it is the beginning pair or the end pair of the comment for this I used the global variable ignore
+			{
+				if(ignore==0)
+				//if ignore is 0 then this is the beginning pair and sets ignore to 1 and commentFlag to 1
+				{
+					ignore=1;
+					commentFlag=1;
+				}
+				else if(ignore==1)
+				//if ignore if already 1 then this is the end pair and ignore is set back to one but the comment Flag is set to 1 so the apersand is not put into newLine 
+				{
+					ignore=0;
+					commentFlag=1;
+				}
+			}
+			else
+			//if we got one apersand and then their was not a second one then the appersand will be added to the newLine 
+			{
+				lineCharCount--;
+			}	
+		}
+		if(ignore==0&&commentFlag==0)
+		//if both the ignore and comment flags are 0 then the character c can be added to newLine and the new Line count is incremented
+		{
+			newLine[newLineCharCount]=c;
+			newLineCharCount++;	
 
+		}
+		//the orginal line count is increased on every iteration
+		lineCharCount++;
+		c=line[lineCharCount];
+		if(commentFlag==1&&ignore==0)
+		//if the comment flag and ignore are different then comment flag is set to zero this is set so the first apersand or the last apersand are not added to newLine
+		{
+			commentFlag=0;
+		}
+	}
+	//a new line character is added to the end of newLine 
+	newLine[newLineCharCount]='\n';	
+	//the newLine is returned 
+	return newLine;
 }
