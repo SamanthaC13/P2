deacffe28bbb05998e45bb3f7131ff7d70a0f14f
SamanthaC13 <sc3z7@umsystem.edu>
2021-11-03 17:33:56 -0500

    Updated input in driver.c

---
+++ driver.c
@@ -0,0 +1,51 @@
+/*Name: Samantha Craig
+Class:CS4280
+   Project P2
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"token.h"
+#include "scanner.h"
+char* filterLine(char*);
+int ignore=0;
+int main(int argc, char**argv)
+{
+	char* filename;                   
+	int option=0;
+	//This option if the user only entered the Program name without a file
+	if(argc==1)
+	{
+		char character;
+		FILE* temp;
+		option=1;
+		temp=fopen("temp.txt","w");
+		//Takes in the Keyboard input and writes it to a file I call temp.txt
+		while((character=getc(stdin))!=EOF)
+		{
+			fputc(character,temp);
+		}
+		//Writes the end of file to the temp file I created for the keyboard input
+		fputc('\n',temp);
+		fputc('\0',temp);
+		fclose(temp);
+		filename=malloc(sizeof(char)*10);
+		strcpy(filename,"temp.txt");
+        }
+	else
+	{
+		//filename is set to the file the user entered with the implict extension .fs
+		filename=argv[1];
+		filename=strcat(filename,".fs");
+		
+	}
+	FILE* input;
+	char* lastLine;
+	//file is opened for reading 
+	input=fopen(filename,"r");
+	if(input==NULL)
+	{
+		//Error is printed if input file is  unable to be opened
+		fprintf(stderr,"Error opening input file in TestScanner");
+		exit(1);
+	}
