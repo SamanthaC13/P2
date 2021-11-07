3b92695a9ac7d74b8229a69a59746ff1d9527a26
SamanthaC13 <sc3z7@umsystem.edu>
2021-11-07 12:24:11 -0600

    Update parser.h

---
+++ parser.h
@@ -0,0 +1,11 @@
+void parser(char*);
+char* filterLine(char*);
+char* getNewLine(char*);
+struct tokenType filterToken(struct tokenType);
+void vars();
+void block();
+void stats();
+void stat();
+void mstat();
+void in();
+void out();
